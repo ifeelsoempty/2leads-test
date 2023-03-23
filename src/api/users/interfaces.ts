af8599922b14ee21d44636b2432c7d50523fc42a
ifeelsoempty <brayan.1702@mail.ru>
2023-03-24 00:30:12 +0600

    Updates

--- src/api/users/interfaces.ts
+++
@@ -1,9 +0,0 @@
-export interface UserFromServer {
-  id: number;
-  name: string;
-  email: string;
-  phone: string;
-  address: {
-    city: string;
-  };
-}
--- src/api/users/users.ts
+++
@@ -1,9 +0,0 @@
-import { UserFromServer } from "./interfaces";
-
-const USERS_API_URL = "https://jsonplaceholder.typicode.com/users";
-
-export const fetchUsers = async (): Promise<UserFromServer[]> => {
-  const response = await fetch(USERS_API_URL);
-  const data = await response.json();
-  return data;
-};
